0cdc1edfa883b3c8278ede4f2520341d086d0682
arjunloll <lallarjun@gmail.com>
2017-02-21 02:43:09 -0500

    world map data visualization

---
+++ country_codes.py
@@ -0,0 +1,10 @@
+from pygal.maps.world import COUNTRIES
+from pygal_maps_world import i18n
+
+def get_country_code(country_name):
+    """Return the Pygal 2-digit country code for the given country"""
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+        #If country wasn't found return none
+    return None
